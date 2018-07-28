de60b30b335d42445c1b55a583142bc9d57a7814
Vaibhav Sinha <cs15btech11034@iith.ac.in>
2018-07-28 23:01:38 +0530

    Few small changes

--- Scalogram.m
+++ Scalogram.m
@@ -1,6 +1,6 @@
-% Use the code above to plot the scalogram. Change the path of the wav file, locally.
+% Use the code above to plot the scalogram.
 % The second argument in the cwt function specifies the mother wavelet to be used. 
 % Use either 'morse', 'amor' or 'bump' for Morse, Morlet and Bump wavelet respectiverly.
 
-[y, Fs] = audioread('/home/vaibhav/SpeechEmotionAnalysis/Audio-features/audios/bogo.wav');
+[y, Fs] = audioread('/audios/bogo.wav');
 cwt(y, 'bump', Fs)
